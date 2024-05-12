87d50548051373a579a3871b229ad1e343120b2e
Haneensherif <haneen.sherif@msa.edu.eg>
2024-05-12 18:00:45 +0300

    Update Menu.cpp

--- Project-1/Menu.cpp
+++ Project-1/Menu.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int num;
 string newpass;
 void Menu::displayMainMenu() {
+	system("cls");
 	cout << "\t\tMain Menu\n1.Add Record\n2.View Record\n3.Edit Record\n4.Delete Record\n5.Make New Password\n6.Display New Password\n7.Save Record To File\n8.Read Record From File\n9.Update File\n10.Quit\n" << endl;
 	getUserChoice();
 
@@ -32,27 +33,36 @@ void Menu::getUserChoice() {
 	case 1:
 		addRecord();
 		saveToFile();
+		system("cls");
 		break;
 	case 2:
 		viewRecord();
+		system("cls");
 		break;
 	case 3:
+		int x;
 		editRecord();
 		updateFile();
+		cout<<"if you want to clear the screen enter 0 "<<endl;
+		cin>>x
+		if(x==0){
+			system("cls");}
 		break;
 	case 4:
 		deleteRecord();
 		updateFile();
+		system("cls");
 		break;
 	case 5:
-		system("cls");
 		cout << "Enter the new password " << endl;
 		cin.ignore();
 		getline(cin,newpass);
 		setPassword(newpass);
+		system("cls");
 		break;
 	case 6:
 		getPassword();
+		
 		break;
 	case 7:
 		saveToFile();
@@ -65,6 +75,11 @@ void Menu::getUserChoice() {
 		break;
 	case 10: 
 		diplayRecordList();
+		int x ;
+		cout<<"if you want to clear the screen enter 0 "<<endl;
+		cin>>x
+		if(x==0){
+			system("cls");}
 		
 	case 11:
 		cout << "quit main menu";
